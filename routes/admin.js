9770ac5b15f457be4831969dc15aafa489e0d038
Muhammed Safvan MP <134296048+MuhammedSafvanMP@users.noreply.github.com>
2025-11-11 22:43:07 +0530

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -3,15 +3,12 @@ const router = express.Router();
 const Controller = require('../controllers/admin');
 const { uploadImageSingle } = require('../lib/multer');
 
-
-router.post('/',  Controller.create); 
+router.post("/login", Controller.login);
+router.post('/', Controller.create);
 router.get('/super-admins/:id', Controller.getAll);
+router.patch("/block/:id", Controller.block);
 router.get('/:id', Controller.get);
 router.put('/:id', Controller.update);
 router.delete('/:id', Controller.delete);
-router.patch("/block/:id", Controller.block);
-router.post("/login", Controller.login);
-
-
 
 module.exports = router;
